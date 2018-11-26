389a9095154bb53da3eceb3b770a8fdc0ace0aba
novakvova <novakvova@gmail.com>
2018-11-26 16:28:48 +0200

    Begin app

---
+++ WebCropImage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebCropImage.Startup))]
+namespace WebCropImage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
